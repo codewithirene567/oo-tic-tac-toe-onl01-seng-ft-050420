597d9a62a853592a653c4e96d29bac932aeb8ccb
Irene Scott <irenejinscott@gmail.com>
2020-05-16 23:03:04 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,90 @@
+require "pry"
+
+class TicTacToe
+  attr_accessor :board
+  
+  WIN_COMBINATIONS = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ]
+  
+  def initialize
+    @board = [" "," "," "," "," "," "," "," "," "]
+  end
+  
+  def display_board
+    game = @board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input) #input = "1"
+    input.to_i - 1
+  end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
+  
+  def position_taken?(index)
+    #when an index value is passed for a move
+    ((@board[index] == "X" || @board[index] == "O"))
+  end
+  
+    #check to see if that position on the @board is open or if it has an x or o
+    #if it is free, then it should be FALSE, else it will remain true
+    
+   def valid_move?(index)
+     if !position_taken?(index) && index >= 0 && index <=8
+       true
+     else
+       false
+   end
+   end
+  
+   def turn 
+     puts "Please enter a position between 1-9:"
+     input = gets.strip
+     index = input_to_index(input)
+     person = current_player
+     if valid_move?(index)
+       move(index, person)
+       display_board
+     else 
+       turn
+    end
+    end
+    
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+    
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end
+  
+  def won?
+  WIN_COMBINATIONS.detect do |winning_code|
+    if (@board|winning_code|
+      winning_code
+      else 
+      false
+  end
+  end
+    
+   
+  
+    #Calling the gets method captures the last thing the user typed into the terminal. #Whenever your program makes a call to gets, it will freeze and wait for user input
+    
+end
+  
+  
