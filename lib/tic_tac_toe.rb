bd65683b3ec993f633e54f034e8999d2da364b3a
Irene Scott <irenejinscott@gmail.com>
2020-05-17 00:34:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -116,19 +116,7 @@ class TicTacToe
      puts "The game is a draw"
    end
    
-   var = 7
-  
-# using until loop 
-# here do is optional 
-until var == 11 do
-  
-  # code to be executed 
-  puts var * 10
-  var = var + 1
-    
-# here loop ends 
-end
-  
+   
     
 
    
